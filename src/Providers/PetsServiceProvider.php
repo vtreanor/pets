32e0dc7bc0e815861fbfd3aff4dc4bebe06a870e
Vincent Treanor <treanorv@gmail.com>
2020-03-19 07:59:32 +0000

    First commit

---
+++ src/Providers/PetsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vtreanor\Pets\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PetsServiceProvider extends ServiceProvider{
+  public function boot() {
+//    dd("Loading PetsServiceProvider, so there!");
+
+  }
+
+  public function register() {
+
+  }
+}
